dc73e90bcbfd0e1411ed75c4a53eb41c9517e52c
Tautvydas <banys.tautvydas@gmail.com>
2021-03-09 10:04:30 +0200

    First version

---
+++ test.js
@@ -0,0 +1,24 @@
+const alert = require('./index.js');
+
+alert();
+
+alert({
+  type: `success`,
+  msg: `All done!`,
+  name: 'DONE'
+});
+
+alert({
+  type: `warning`,
+  msg: `You didn't add something!`
+});
+
+alert({
+  type: `info`,
+  msg: `Awais is awesome!`
+});
+
+alert({
+  type: `error`,
+  msg: `Something went wrong!`
+});
